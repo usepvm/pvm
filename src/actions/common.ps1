a3394bdfdfb0433f235528867ee1c930d1524d74
faissaloux <fwahabali@gmail.com>
2025-07-25 17:39:02 +0100

    fix Is-PHP-Installed return

--- src/actions/common.ps1
+++ src/actions/common.ps1
@@ -38,14 +38,15 @@ function Is-PVM-Setup {
 
 function Is-PHP-Installed {
     param ($version)
+    $return = $false
 
     Get-ChildItem -Path $PHP_VERSIONS_PATH -Directory | ForEach-Object {
         $split = $_.ToString().split("-")
 
         if ($split.Count -gt 1 -and $version -eq $split[1]) {
-            return $true
+            $return = $true
         }
     }
 
-    return $false
+    return $return
 }

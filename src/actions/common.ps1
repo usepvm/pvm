12810e00a2355c5a092c4a0107bc91f1fb15909f
faissaloux <fwahabali@gmail.com>
2025-07-25 17:29:19 +0100

    use GLOBAL

--- src/actions/common.ps1
+++ src/actions/common.ps1
@@ -39,7 +39,7 @@ function Is-PVM-Setup {
 function Is-PHP-Installed {
     param ($version)
 
-    Get-ChildItem -Path "$STORAGE_PATH\php\versions" -Directory | ForEach-Object {
+    Get-ChildItem -Path $PHP_VERSIONS_PATH -Directory | ForEach-Object {
         $split = $_.ToString().split("-")
 
         if ($split.Count -gt 1 -and $version -eq $split[1]) {

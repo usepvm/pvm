dffc68a6f20b07143be70ed3a9a58ccdd623ed67
Driss <drissboumlik.dev@gmail.com>
2025-07-28 11:29:25 +0100

    Remove unused const

--- src/core/config.ps1
+++ src/core/config.ps1
@@ -10,11 +10,10 @@ $Global:PVMEntryPoint = "$PVMRoot\src\pvm.ps1"
 # Storage paths
 $Global:STORAGE_PATH = "$PVMRoot\storage"
 $Global:DATA_PATH = "$STORAGE_PATH\data"
-$Global:LOG_PATH = "$STORAGE_PATH\logs"
 
 # Log paths
-$Global:LOG_ERROR_PATH = "$LOG_PATH\error.log"
-$Global:PATH_VAR_BACKUP_PATH = "$LOG_PATH\path.bak.log"
+$Global:LOG_ERROR_PATH = "$STORAGE_PATH\logs\error.log"
+$Global:PATH_VAR_BACKUP_PATH = "$STORAGE_PATH\logs\path.bak.log"
 
 # Environment variable names
 $Global:PATH_VAR_BACKUP_NAME = "Path.bak"

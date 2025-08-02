3d03157f3b42db988fea4d07a493f1478cfdb1f0
Driss <drissboumlik.dev@gmail.com>
2025-08-02 19:21:00 +0100

    Print tested file

--- src/actions/test.ps1
+++ src/actions/test.ps1
@@ -10,6 +10,9 @@ function Run-Tests {
         $tests = Get-ChildItem "$PVMRoot\tests\*.tests.ps1"
         $tests | ForEach-Object { 
             try {
+                Write-Host "`n----------------------------------------------------------------"
+                Write-Host "- Running test: $($_.Name)"
+                Write-Host "----------------------------------------------------------------"
                 $fileName = $_.FullName
                 $config.Run.Path = $fileName
                 Invoke-Pester -Configuration $config
@@ -17,6 +20,7 @@ function Run-Tests {
                 $logged = Log-Data -logPath $LOG_ERROR_PATH -message "Run-Tests: Failed to run test: $fileName" -data $_.Exception.Message
                 Write-Host "`n- Failed to run test: $fileName"
             }
+            Write-Host "`n"
         }
     } catch {
         $logged = Log-Data -logPath $LOG_ERROR_PATH -message "Run-Tests: Failed to run tests" -data $_.Exception.Message

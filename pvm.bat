ba3522e23caccca310823a19f38ac29c3343a2de
Driss <drissboumlik.dev@gmail.com>
2025-08-29 21:17:49 +0100

    Use pwsh if available, fallback to powershell

--- pvm.bat
+++ pvm.bat
@@ -3,6 +3,12 @@ setlocal
 
 rem Run PowerShell with the provided arguments
 SET FILE_TARGET=%~dp0\src\pvm.ps1
-powershell -ExecutionPolicy Bypass -File "%FILE_TARGET%" %*
+
+where pwsh >nul 2>&1
+if %ERRORLEVEL%==0 (
+    pwsh -ExecutionPolicy Bypass -File "%FILE_TARGET%" %*
+) else (
+    powershell -ExecutionPolicy Bypass -File "%FILE_TARGET%" %*
+)
 
 endlocal

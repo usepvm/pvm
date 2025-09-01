740e6c39ef7ac25d5ec641f66c6ffd0fe681f664
Driss <drissboumlik.dev@gmail.com>
2025-09-01 17:36:15 +0100

    Handle empty option input

--- pvm.bat
+++ pvm.bat
@@ -4,11 +4,18 @@ setlocal
 rem Run PowerShell with the provided arguments
 SET FILE_TARGET=%~dp0\src\pvm.ps1
 
+rem If no arguments provided, show help
+if "%~1"=="--" (
+    set ARGS=help
+) else (
+    set ARGS=%*
+)
+
 where pwsh >nul 2>&1
 if %ERRORLEVEL%==0 (
-    pwsh -ExecutionPolicy Bypass -File "%FILE_TARGET%" %*
+    pwsh -ExecutionPolicy Bypass -File "%FILE_TARGET%" %ARGS%
 ) else (
-    powershell -ExecutionPolicy Bypass -File "%FILE_TARGET%" %*
+    powershell -ExecutionPolicy Bypass -File "%FILE_TARGET%" %ARGS%
 )
 
 endlocal

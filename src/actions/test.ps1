5d0439c1627cd45c342195a54dac2c7c65c1a301
Driss <drissboumlik.dev@gmail.com>
2025-09-06 13:49:49 +0100

    Refactor test options and output

--- src/actions/test.ps1
+++ src/actions/test.ps1
@@ -1,6 +1,6 @@
 
 function Get-Tests-Files {
-    param ($tests)
+    param ($tests = $null)
     
     if ($tests) {
         $tests = $tests | ForEach-Object {
@@ -22,14 +22,18 @@ function Get-Tests-Files {
 } 
 
 function Run-Test-File {
-    param ($config, $file, $verbosity, $coverage = $false)
+    param ($config, $file, $options = $null)
     
     if (-not (Test-Path $file.FullName)) {
-        return @{ Name = $file.Name; Count = 1; Message = "File not found!" }
+        return @{ code = -1; Name = $file.Name; FailedCount = 0; Message = "File not found!" }
+    }
+    
+    if (-not $options) {
+        $options = @{ coverage = $false; target = 75 }
     }
 
     $coveredFile = ""
-    if ($coverage) {
+    if ($options.coverage) {
         $PVMRootDirectory = (Resolve-Path "$PSScriptRoot\..\..").Path
         $covered = Get-ChildItem -Path "$PVMRootDirectory\src" -Recurse -Filter "*.ps1"
         $covered = $covered | Where-Object {
@@ -38,9 +42,10 @@ function Run-Test-File {
             
         $config.CodeCoverage.Enabled = $true
         $config.CodeCoverage.Path = $covered.FullName
-        $config.CodeCoverage.OutputPath = "$PVMRootDirectory\coverage\$($file.Name).xml"
+        $config.CodeCoverage.OutputPath = "$PVMRootDirectory\storage\coverage\$($file.Name).xml"
         $config.CodeCoverage.OutputFormat = 'JaCoCo' # 'CoverageGutters'
         $config.CodeCoverage.OutputEncoding = 'UTF8'
+        $config.CodeCoverage.CoveragePercentTarget = $options.target
         $coveredFile = "(covers: $($covered.Name))"
     }
     
@@ -52,42 +57,45 @@ function Run-Test-File {
     $config.Run.Path = $file.FullName
     $config.Run.PassThru = $true
     $testResult = Invoke-Pester -Configuration $config
+    $coveragePercent = $testResult.CodeCoverage.CoveragePercent.ToString('00.00')
     if ($LASTEXITCODE -ne 0) {
-        return @{ Name = $file.Name; Count = $($testResult.FailedCount); Message = "$($testResult.FailedCount) tests" }
+        return @{ code = -1; Name = $file.Name; FailedCount = $($testResult.FailedCount); Message = "Coverage: $coveragePercent% / Failed: $($testResult.FailedCount)" }
     }
     
-    return $null
+    return @{ code = 0; Name = $file.Name; FailedCount = 0; Message = "Coverage: $coveragePercent%" }
 }
 
 function Run-Tests {
-    param ($verbosity = "Normal", $tests = $null, $tag = $null, $coverage = $false)
+    param ($tests = $null, $options = $null)
     
     try {
+        if (-not $options) {
+            $options = @{ verbosity = "Normal"; coverage = $false; tag = $null; target = 75 }
+        }
+        
         $verbosityOptions = @('None', 'Normal', 'Detailed', 'Diagnostic')
-        if ($verbosityOptions -notcontains $verbosity) {
+        if ($verbosityOptions -notcontains $options.verbosity) {
             Write-Host "`nInvalid verbosity option. Allowed values are: $($verbosityOptions -join ', ')" -ForegroundColor DarkYellow
             return -1
         }
 
         $config = New-PesterConfiguration
-        $config.Output.Verbosity = $verbosity
+        $config.Output.Verbosity = $options.verbosity
         
         $tests = Get-Tests-Files -tests $tests
         
-        if ($null -ne $tag) {
-            $config.Filter.Tag = $tag
+        if ($null -ne $options.tag) {
+            $config.Filter.Tag = $options.tag
         }
         
         $result = @{ code = 0; message = "Tests completed successfully."; color = "DarkGreen" }
-        $testFailedDetails = @()
-        Write-Host "`nRunning tests with verbosity: $verbosity" -ForegroundColor Cyan
+        $testSummary = @()
+        Write-Host "`nRunning tests with verbosity: $($options.verbosity)" -ForegroundColor Cyan
         $tests | ForEach-Object { 
             try {
                 $file = $_
-                $testResult = Run-Test-File -config $config -file $file -verbosity $verbosity -coverage $coverage
-                if ($testResult -and $testResult.Count -gt 0) {
-                    $testFailedDetails += $testResult
-                }
+                $testResult = Run-Test-File -config $config -file $file -options $options
+                $testSummary += $testResult
             } catch {
                 $logged = Log-Data -data @{
                     header = "$($MyInvocation.MyCommand.Name) - Failed to run test: $($file.FullName)"
@@ -98,24 +106,21 @@ function Run-Tests {
             }
         }
         
-        if ($testFailedDetails.Count -gt 0) {
-            $totalFailedTests = $testFailedDetails | ForEach-Object { $_.Count } | Measure-Object -Sum | Select-Object -ExpandProperty Sum
-            $message = " Files failed to run: $($testFailedDetails.Count)  |  Total failed tests: $totalFailedTests"
-            $maxFileNameLength = ($testFailedDetails.Name | Measure-Object -Maximum Length).Maximum
+        $message = "`n`nTest Results Summary:`n"
+        if ($testSummary.Count -gt 0) {
+            $totalFailedTests = $testSummary | Where-Object { $_.code -ne 0 } | ForEach-Object { $_.FailedCount } | Measure-Object -Sum | Select-Object -ExpandProperty Sum
+            $message += "`n Files tested : $($testSummary.Count)  |  Total failed tests: $totalFailedTests`n"
+            $maxFileNameLength = ($testSummary.Name | Measure-Object -Maximum Length).Maximum
             $maxLineLength = $maxFileNameLength + 10  # padding
             
-            $testFailedDetails | ForEach-Object {
+            $testSummary | ForEach-Object {
                 $dotsCount = $maxLineLength - $_.Name.Length
                 if ($dotsCount -lt 0) { $dotsCount = 0 }
                 $dots = '.' * $dotsCount
                 $message += "`n  - $($_.Name) $dots $($_.Message)"
             }
-            $result = @{ code = 1; message = $message; color = "DarkGray" }
+            $result = @{ code = 1; message = $message }
         }
-        
-        $message = "`nTest Results Summary:"
-        $message += "`n=======================`n`n"
-        $result.message = $message + $result.message
         
         return $result
     } catch {

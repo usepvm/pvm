bc0812f9afb10301d8278ab005de8b4300486fbb
Driss <drissboumlik.dev@gmail.com>
2026-05-31 00:39:40 +0100

    Edit formatter

--- src/format.ps1
+++ src/format.ps1
@@ -1,4 +1,4 @@
-
+﻿
 
 # Load configuration
 . $PSScriptRoot\core\config.ps1
@@ -16,14 +16,45 @@ if (-not (Get-Module PSScriptAnalyzer -ListAvailable)) {
 # Import the module
 Import-Module PSScriptAnalyzer -Force
 
+$settings = @{
+    IncludeRules = @(
+        'PSUseConsistentIndentation'
+        'PSUseConsistentWhitespace'
+        'PSPlaceOpenBrace'
+        'PSPlaceCloseBrace'
+        'PSAlignAssignmentStatement'
+        'PSAvoidTrailingWhitespace'
+        'PSAvoidSemicolonsAsLineTerminators'
+        'PSPossibleIncorrectComparisonWithNull'
+        'PSPossibleIncorrectUsageOfAssignmentOperator'
+        'PSAvoidLongLines'
+    )
+
+    Rules = @{
+        PSUseConsistentIndentation = @{
+            Kind = 'space'
+            IndentationSize = 4
+        }
+
+        PSUseConsistentWhitespace = @{
+            CheckInnerBrace = $true
+            CheckOpenBrace = $true
+            CheckOpenParen = $true
+            CheckOperator = $true
+            CheckPipe = $true
+            CheckSeparator = $true
+        }
+    }
+}
+
 # Format files in src/ and tests/
 @("$PVMRoot\src", "$PVMRoot\tests") | ForEach-Object {
     $directory = $_
     Get-ChildItem "$directory\*.ps1" -Recurse | ForEach-Object {
         try {
-            Write-Host "Formatting: $($_.FullName)"
-            # Use Invoke-ScriptAnalyzer with formatting
-            Invoke-ScriptAnalyzer -Path $_.FullName -Fix -ExcludeRule PSUseSingularNouns
+            Write-Host "`nFormatting: $($_.FullName)"
+
+            Invoke-ScriptAnalyzer -Path $_.FullName -Fix -Settings $settings
             $formatted++
         } catch {
             $errors += "Error formatting file: $($_.FullName) - $_"

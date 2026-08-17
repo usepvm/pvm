e44aefcef7888ac3ee37b714667d9fae60d96d04
Driss <drissboumlik.dev@gmail.com>
2026-08-17 21:24:59 +0100

    Merge branch 'master' into Refactor-New-Item

--- src/helpers/io.ps1
+++ src/helpers/io.ps1
@@ -102,6 +102,7 @@ function New-Directory {
 
         return 0
     } catch {
+        $null = Add-LogEntry -data @{ header = "$($MyInvocation.MyCommand.Name) - Failed to create directory '$path'"; exception = $_ }
         return -1
     }
 }
@@ -121,7 +122,7 @@ function New-File {
 
         return 0
     } catch {
-        $null = Add-LogEntry -data @{ header = "$($MyInvocation.MyCommand.Name) - Failed to create directory '$path'"; exception = $_ }
+        $null = Add-LogEntry -data @{ header = "$($MyInvocation.MyCommand.Name) - Failed to create file '$path'"; exception = $_ }
         return -1
     }
 }

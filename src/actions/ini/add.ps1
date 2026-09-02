a8730cfd0f8c2b24ec7dd16da6efad25e64cf395
Driss <drissboumlik.dev@gmail.com>
2026-09-02 17:20:18 +0100

    Check if extension is not configured

--- src/actions/ini/add.ps1
+++ src/actions/ini/add.ps1
@@ -89,8 +89,8 @@ function Add-MissingPHPExtensionToIni {
             return -1
         }
 
-        $matchesList = Get-MatchingPHPExtensionsStatus -iniPath $iniPath -extName $extFileName -includeIniOnly $true -addToIniFileIfMissing $false
-        if ($matchesList.Length -gt 0) {
+        $matchesList = (Get-MatchingPHPExtensionsStatus -iniPath $iniPath -extName $extFileName -includeIniOnly $true -addToIniFileIfMissing $false) | Select-Object -First 1 
+        if ($matchesList.Length -gt 0 -and $matchesList.LineNumber -gt 0) {
             Show-Warning -message "- Extension '$extFileName' already exists in php.ini"
             return 0
         }

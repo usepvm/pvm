2166ded80240c536f344bc44566574b57757b0b8
Driss <drissboumlik.dev@gmail.com>
2026-09-06 15:38:37 +0100

    Fix code style

--- tests/core/router.tests.ps1
+++ tests/core/router.tests.ps1
@@ -9,17 +9,17 @@ BeforeAll {
 Describe "Get-HelpAction" {
     It "Should return the help action" {
         Mock Invoke-Help { return 0 }
-        
+
         $helpAction = Get-HelpAction
-        
+
         $helpAction | Should -BeOfType [hashtable]
         $helpAction.Keys | Should -Contain 'command'
         $helpAction.Keys | Should -Contain 'description'
         $helpAction.Keys | Should -Contain 'usage'
         $helpAction.Keys | Should -Contain 'action'
-        
+
         $code = & $helpAction.action -arguments @()
-        
+
         $code | Should -Be 0
         Should -Invoke Invoke-Help -Times 1
     }
@@ -28,17 +28,17 @@ Describe "Get-HelpAction" {
 Describe "Get-VersionAction" {
     It "Should return the version action" {
         Mock Invoke-Version { return 0 }
-        
+
         $versionAction = Get-VersionAction
-        
+
         $versionAction | Should -BeOfType [hashtable]
         $versionAction.Keys | Should -Contain 'command'
         $versionAction.Keys | Should -Contain 'description'
         $versionAction.Keys | Should -Contain 'usage'
         $versionAction.Keys | Should -Contain 'action'
-        
+
         $code = & $versionAction.action -arguments @()
-        
+
         $code | Should -Be 0
         Should -Invoke Invoke-Version -Times 1
     }
@@ -47,17 +47,17 @@ Describe "Get-VersionAction" {
 Describe "Get-SetupAction" {
     It "Should return the setup action" {
         Mock Invoke-Setup { return 0 }
-        
+
         $setupAction = Get-SetupAction
-        
+
         $setupAction | Should -BeOfType [hashtable]
         $setupAction.Keys | Should -Contain 'command'
         $setupAction.Keys | Should -Contain 'description'
         $setupAction.Keys | Should -Contain 'usage'
         $setupAction.Keys | Should -Contain 'action'
-        
+
         $code = & $setupAction.action -arguments @()
-        
+
         $code | Should -Be 0
         Should -Invoke Invoke-Setup -Times 1
     }
@@ -66,17 +66,17 @@ Describe "Get-SetupAction" {
 Describe "Get-CurrentAction" {
     It "Should return the current action" {
         Mock Invoke-Current { return 0 }
-        
+
         $currentAction = Get-CurrentAction
-        
+
         $currentAction | Should -BeOfType [hashtable]
         $currentAction.Keys | Should -Contain 'command'
         $currentAction.Keys | Should -Contain 'description'
         $currentAction.Keys | Should -Contain 'usage'
         $currentAction.Keys | Should -Contain 'action'
-        
+
         $code = & $currentAction.action -arguments @()
-        
+
         $code | Should -Be 0
         Should -Invoke Invoke-Current -Times 1
     }
@@ -85,17 +85,17 @@ Describe "Get-CurrentAction" {
 Describe "Get-ListAction" {
     It "Should return the list action" {
         Mock Invoke-List { return 0 }
-        
+
         $listAction = Get-ListAction
-        
+
         $listAction | Should -BeOfType [hashtable]
         $listAction.Keys | Should -Contain 'command'
         $listAction.Keys | Should -Contain 'description'
         $listAction.Keys | Should -Contain 'usage'
         $listAction.Keys | Should -Contain 'action'
-        
+
         $code = & $listAction.action -arguments @('available')
-        
+
         $code | Should -Be 0
         Should -Invoke Invoke-List -Times 1
     }
@@ -104,17 +104,17 @@ Describe "Get-ListAction" {
 Describe "Get-InstallAction" {
     It "Should return the install action" {
         Mock Invoke-Install { return 0 }
-        
+
         $installAction = Get-InstallAction
-        
+
         $installAction | Should -BeOfType [hashtable]
         $installAction.Keys | Should -Contain 'command'
         $installAction.Keys | Should -Contain 'description'
         $installAction.Keys | Should -Contain 'usage'
         $installAction.Keys | Should -Contain 'action'
-        
+
         $code = & $installAction.action -arguments @('8.2.0')
-        
+
         $code | Should -Be 0
         Should -Invoke Invoke-Install -Times 1
     }
@@ -123,17 +123,17 @@ Describe "Get-InstallAction" {
 Describe "Get-UseAction" {
     It "Should return the use action" {
         Mock Invoke-Use { return 0 }
-        
+
         $useAction = Get-UseAction
-        
+
         $useAction | Should -BeOfType [hashtable]
         $useAction.Keys | Should -Contain 'command'
         $useAction.Keys | Should -Contain 'description'
         $useAction.Keys | Should -Contain 'usage'
         $useAction.Keys | Should -Contain 'action'
-        
+
         $code = & $useAction.action -arguments @('8.2.0')
-        
+
         $code | Should -Be 0
         Should -Invoke Invoke-Use -Times 1
     }
@@ -142,17 +142,17 @@ Describe "Get-UseAction" {
 Describe "Get-UninstallAction" {
     It "Should return the uninstall action" {
         Mock Invoke-Uninstall { return 0 }
-        
+
         $uninstallAction = Get-UninstallAction
-        
+
         $uninstallAction | Should -BeOfType [hashtable]
         $uninstallAction.Keys | Should -Contain 'command'
         $uninstallAction.Keys | Should -Contain 'description'
         $uninstallAction.Keys | Should -Contain 'usage'
         $uninstallAction.Keys | Should -Contain 'action'
-        
+
         $code = & $uninstallAction.action -arguments @('8.2.0')
-        
+
         $code | Should -Be 0
         Should -Invoke Invoke-Uninstall -Times 1
     }
@@ -161,17 +161,17 @@ Describe "Get-UninstallAction" {
 Describe "Get-IniAction" {
     It "Should return the ini action" {
         Mock Invoke-Ini { return 0 }
-        
+
         $iniAction = Get-IniAction
-        
+
         $iniAction | Should -BeOfType [hashtable]
         $iniAction.Keys | Should -Contain 'command'
         $iniAction.Keys | Should -Contain 'description'
         $iniAction.Keys | Should -Contain 'usage'
         $iniAction.Keys | Should -Contain 'action'
-        
+
         $code = & $iniAction.action -arguments @('set', 'memory_limit=256M')
-        
+
         $code | Should -Be 0
         Should -Invoke Invoke-Ini -Times 1
     }
@@ -180,17 +180,17 @@ Describe "Get-IniAction" {
 Describe "Get-ProfileAction" {
     It "Should return the profile action" {
         Mock Invoke-Profile { return 0 }
-        
+
         $profileAction = Get-ProfileAction
-        
+
         $profileAction | Should -BeOfType [hashtable]
         $profileAction.Keys | Should -Contain 'command'
         $profileAction.Keys | Should -Contain 'description'
         $profileAction.Keys | Should -Contain 'usage'
         $profileAction.Keys | Should -Contain 'action'
-        
+
         $code = & $profileAction.action -arguments @('8.2.0')
-        
+
         $code | Should -Be 0
         Should -Invoke Invoke-Profile -Times 1
     }
@@ -199,17 +199,17 @@ Describe "Get-ProfileAction" {
 Describe "Get-InfoAction" {
     It "Should return the info action" {
         Mock Invoke-Info { return 0 }
-        
+
         $infoAction = Get-InfoAction
-        
+
         $infoAction | Should -BeOfType [hashtable]
         $infoAction.Keys | Should -Contain 'command'
         $infoAction.Keys | Should -Contain 'description'
         $infoAction.Keys | Should -Contain 'usage'
         $infoAction.Keys | Should -Contain 'action'
-        
+
         $code = & $infoAction.action -arguments @()
-        
+
         $code | Should -Be 0
         Should -Invoke Invoke-Info -Times 1
     }
@@ -218,17 +218,17 @@ Describe "Get-InfoAction" {
 Describe "Get-AliasesAction" {
     It "Should return the aliases action" {
         Mock Invoke-Aliases { return 0 }
-        
+
         $aliasesAction = Get-AliasesAction
-        
+
         $aliasesAction | Should -BeOfType [hashtable]
         $aliasesAction.Keys | Should -Contain 'command'
         $aliasesAction.Keys | Should -Contain 'description'
         $aliasesAction.Keys | Should -Contain 'usage'
         $aliasesAction.Keys | Should -Contain 'action'
-        
+
         $code = & $aliasesAction.action -arguments @()
-        
+
         $code | Should -Be 0
         Should -Invoke Invoke-Aliases -Times 1
     }
@@ -237,37 +237,36 @@ Describe "Get-AliasesAction" {
 Describe "Get-LogAction" {
     It "Should return the log action" {
         Mock Invoke-Log { return 0 }
-        
+
         $logAction = Get-LogAction
-        
+
         $logAction | Should -BeOfType [hashtable]
         $logAction.Keys | Should -Contain 'command'
         $logAction.Keys | Should -Contain 'description'
         $logAction.Keys | Should -Contain 'usage'
         $logAction.Keys | Should -Contain 'action'
-        
+
         $code = & $logAction.action -arguments @()
-        
+
         $code | Should -Be 0
         Should -Invoke Invoke-Log -Times 1
     }
 }
 
-
 Describe "Get-RepairAction" {
     It "Should return the repair action" {
         Mock Invoke-Repair { return 0 }
-        
+
         $repairAction = Get-RepairAction
-        
+
         $repairAction | Should -BeOfType [hashtable]
         $repairAction.Keys | Should -Contain 'command'
         $repairAction.Keys | Should -Contain 'description'
         $repairAction.Keys | Should -Contain 'usage'
         $repairAction.Keys | Should -Contain 'action'
-        
+
         $code = & $repairAction.action -arguments @()
-        
+
         $code | Should -Be 0
         Should -Invoke Invoke-Repair -Times 1
     }
@@ -276,17 +275,17 @@ Describe "Get-RepairAction" {
 Describe "Get-CacheAction" {
     It "Should return the cache action" {
         Mock Invoke-Cache { return 0 }
-        
+
         $cacheAction = Get-CacheAction
-        
+
         $cacheAction | Should -BeOfType [hashtable]
         $cacheAction.Keys | Should -Contain 'command'
         $cacheAction.Keys | Should -Contain 'description'
         $cacheAction.Keys | Should -Contain 'usage'
         $cacheAction.Keys | Should -Contain 'action'
-        
+
         $code = & $cacheAction.action -arguments @()
-        
+
         $code | Should -Be 0
         Should -Invoke Invoke-Cache -Times 1
     }
@@ -295,37 +294,36 @@ Describe "Get-CacheAction" {
 Describe "Get-UpdateAction" {
     It "Should return the update action" {
         Mock Invoke-Update { return 0 }
-        
+
         $updateAction = Get-UpdateAction
-        
+
         $updateAction | Should -BeOfType [hashtable]
         $updateAction.Keys | Should -Contain 'command'
         $updateAction.Keys | Should -Contain 'description'
         $updateAction.Keys | Should -Contain 'usage'
         $updateAction.Keys | Should -Contain 'action'
-        
+
         $code = & $updateAction.action -arguments @()
-        
+
         $code | Should -Be 0
         Should -Invoke Invoke-Update -Times 1
     }
 }
 
-
 Describe "Get-TestAction" {
     It "Should return the test action" {
         Mock Invoke-Test { return 0 }
-        
+
         $testAction = Get-TestAction
-        
+
         $testAction | Should -BeOfType [hashtable]
         $testAction.Keys | Should -Contain 'command'
         $testAction.Keys | Should -Contain 'description'
         $testAction.Keys | Should -Contain 'usage'
         $testAction.Keys | Should -Contain 'action'
-        
+
         $code = & $testAction.action -arguments @()
-        
+
         $code | Should -Be 0
         Should -Invoke Invoke-Test -Times 1
     }
@@ -334,22 +332,21 @@ Describe "Get-TestAction" {
 Describe "Get-RunAction" {
     It "Should return the run action" {
         Mock Invoke-Run { return 0 }
-        
+
         $runAction = Get-RunAction
-        
+
         $runAction | Should -BeOfType [hashtable]
         $runAction.Keys | Should -Contain 'command'
         $runAction.Keys | Should -Contain 'description'
         $runAction.Keys | Should -Contain 'usage'
         $runAction.Keys | Should -Contain 'action'
-        
+
         $code = & $runAction.action -arguments @()
-        
+
         $code | Should -Be 0
         Should -Invoke Invoke-Run -Times 1
     }
 }
-
 
 Describe "Get-Actions" {
     BeforeEach {
